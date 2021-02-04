49fea6f69044863f87c29242628cd3fcb6b3f546
Krzysztof <krzysztof.hajduk@sarigato.com>
2021-02-04 12:41:16 +0100

    v1 finished

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,19 +17,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test-the-api', function () {
-    return ['message' => 'hello'];
-});
+//Route::get('/posts', [PostController::class, 'index']);
+//Route::get('/posts/{id}', [PostController::class, 'show']);
+//Route::post('/posts/{id}', [PostController::class, 'store']);
+//Route::put('/posts/{id}', [PostController::class, 'update']);
+//Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+
+Route::apiResource('posts', PostController::class);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-// creasting a user route
-Route::get('/user-create', function (Request $request) {
-    User::create([
-        'name' => '',
-        'email' => '',
-        'password' => '',
-    ]);
 });
